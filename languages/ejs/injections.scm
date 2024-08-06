e7797a043550ba8be71496c0106fb78be36a8ecb
Navjot Tomer <navjottomer@gmail.com>
2024-08-06 18:43:22 +0530

    Final changes.

--- languages/ejs/injections.scm
+++ languages/ejs/injections.scm
@@ -1,20 +1,20 @@
-((content) @content
- (#set! "language" "html")
- (#set! "combined"))
-
 ((directive
   (code) @content)
- (#set! "language" "javascript")
- (#set! "combined"))
+ (#set! language "javascript")
+ (#set! combined))
 
 ((output_directive
   (code) @content)
- (#set! "language" "javascript")
- (#set! "combined"))
+ (#set! language "javascript")
+ (#set! combined))
+
+((content) @content
+ (#set! language "html")
+ (#set! combined))
 
 ((graphql_directive
   (code) @content)
- (#set! "language" "graphql")
- (#set! "combined"))
+ (#set! language "graphql")
+ (#set! combined))
 
 ((comment_directive) @comment)
